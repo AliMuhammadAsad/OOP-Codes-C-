6deef04774dc0cc285de09775b8650678ba3fc7f
Ali Muhammad Asad <93670153+AliMuhammadAsad@users.noreply.github.com>
2022-10-20 11:00:43 +0500

    Update Lab8.cpp

--- Lab8.cpp
+++ Lab8.cpp
@@ -145,3 +145,57 @@ int main(){
     cout<<"Area is: "<< decimalArea <<"sq feet"<<endl;
 
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+//DataRate Class
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class DataRate{
+    public:
+    unsigned int rate; string type = "bit/s";
+    DataRate(){rate = 0;}
+    DataRate(unsigned int x){rate = x;}
+    DataRate(unsigned int x, unsigned int exp){rate = x * pow(10, exp);}
+    DataRate(unsigned int x, string unit){
+        if(unit == "bps" || unit == "bit/s" || unit == "b/s") rate = x;
+        else if(unit == "kbps" || unit == "kbit/s" || unit == "kb/s") rate = x * 1000;
+        else if(unit == "Mbps" || unit == "Mbit/s" || unit == "Mb/s") rate = x * 1000000;
+        else if(unit == "Gbps" || unit == "Gbit/s" || unit == "Gb/s") rate = x * 1000000000;
+    }
+    void show(){
+        cout << rate << " " << type << endl;
+    }
+};
+
+
+
+int main(){
+    unsigned int val, mult;
+    string multiple;
+
+    // DataRate sets zero in default constructor
+    DataRate d1;
+
+    cin>>val;
+    // Rate is initilized to integer value supplied as bps
+    DataRate d2(val);
+
+    cin>>val>>mult;
+    // Rate is initialized as val*10^mult bps
+    DataRate d3(val, mult);
+
+
+    cin>>val>>multiple;
+    // Rate is initialized as the val*multiple, where multiple could be bps, kbps, Mbps, Gbps... 
+    DataRate d4(val, multiple);
+
+    // all of the datarates are shown here.
+    d1.show();
+    d2.show();
+    d3.show();
+    d4.show();
+
+    return 0;
+}
